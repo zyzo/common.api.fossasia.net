0630ef700f4ddb286099bcf3a626b12e04033071
zyzo <danghaian168@gmail.com>
2015-06-24 12:35:11 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+Welcome to common.api.fossasia.net ! <br/>
+
+The project's source code can be found at <a href="https://github.com/fossasia/common.api.fossasia.net">fossasia/common.api.fossasia.net</a> on Github.
